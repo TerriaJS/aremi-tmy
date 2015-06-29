1bb1c99b9fcd412796e68b1b951a6f774d8442e1
Mats Henrikson <mats@zakalwe.com>
2015-06-29 12:35:24 +0000

    Testing Cassava CSV parser.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Control.Applicative
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import qualified Data.Vector as V
 
 import ObsTmy
 
+
+data Test = Test
+    { foo    :: !String
+    , bar    :: !Int
+    , woo    :: !Double
+    , wibble :: !String
+    }
+
+
+instance FromNamedRecord Test where
+    parseNamedRecord r = Test <$> r .: "Foo" 
+                             <*> r .: "Bar" 
+                             <*> r .: "Woo" 
+                             <*> r .: "Wibble"
+
+
 main :: IO ()
-main = foobar
+main = do
+    csvData <- BL.readFile "test.csv"
+    case decodeByName csvData of
+        Left err -> putStrLn err
+        Right (_, v) -> V.forM_ v $ \ t ->
+            putStrLn $ foo t ++ "; " ++ show (bar t) ++ "; " ++ show (woo t) ++ "; " ++ wibble t
