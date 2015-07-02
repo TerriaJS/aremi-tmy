de24140fb2ff7510f6af6d18f2fdce113f40dde3
Mats Henrikson <mats@zakalwe.com>
2015-07-02 23:59:51 +1000

    Splitting code to two files.

--- src/Main.hs
+++ src/Main.hs
@@ -2,30 +2,13 @@
 
 module Main where
 
-import Control.Applicative                  ((<$>), (<*>))
 import Control.Monad                        (liftM)
 import qualified Data.ByteString.Lazy as BL (ByteString, readFile)
-import Data.Csv                             (FromNamedRecord, Header, parseNamedRecord, decodeByName, (.:))
-import Data.Text                            (Text, strip)
+import Data.Csv                             (Header, decodeByName, (.:))
 import qualified Data.Vector          as V  (Vector, mapM_, concat)
 import System.Environment                   (getArgs)
 
 import ObsTmy.Import
-
-
-data Site = Site
-    { foo    :: !Text
-    , bar    :: !Int
-    , woo    :: !Double
-    , wibble :: !Text
-    } deriving (Show, Eq, Ord)
-
-
-instance FromNamedRecord Site where
-    parseNamedRecord r = Site <$> r .: "Foo" 
-                              <*> r .: "Bar" 
-                              <*> r .: "Woo" 
-                              <*> r .: "Wibble"
 
 
 main :: IO ()
--- src/ObsTmy/Import.hs
+++ src/ObsTmy/Import.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ObsTmy.Import where
 
+import Control.Applicative                  ((<$>), (<*>))
+import Data.Csv                             (FromNamedRecord, parseNamedRecord, (.:))
+import Data.Text                            (Text, strip)
+
+
+data Site = Site
+    { foo    :: !Text
+    , bar    :: !Int
+    , woo    :: !Double
+    , wibble :: !Text
+    } deriving (Show, Eq, Ord)
+
+
+instance FromNamedRecord Site where
+    parseNamedRecord r = Site <$> r .: "Foo" 
+                              <*> r .: "Bar" 
+                              <*> r .: "Woo" 
+                              <*> r .: "Wibble"
