4a9d1518398fc472cd4fe6d5cb5d4da00bb5cee7
Mats Henrikson <mats@zakalwe.com>
2015-07-02 23:36:17 +1000

    Some cleanup.

--- src/Main.hs
+++ src/Main.hs
@@ -6,22 +6,21 @@ import Control.Applicative                  ((<$>), (<*>))
 import Control.Monad                        (liftM)
 import qualified Data.ByteString.Lazy as BL (ByteString, readFile)
 import Data.Csv                             (FromNamedRecord, Header, parseNamedRecord, decodeByName, (.:))
+import Data.Text                            (Text, strip)
 import qualified Data.Vector          as V  (Vector, mapM_, concat)
 import System.Environment                   (getArgs)
 
-import ObsTmy
 
-
-data Test = Test
-    { foo    :: !String
+data Site = Site
+    { foo    :: !Text
     , bar    :: !Int
     , woo    :: !Double
-    , wibble :: !String
+    , wibble :: !Text
     } deriving (Show, Eq, Ord)
 
 
-instance FromNamedRecord Test where
-    parseNamedRecord r = Test <$> r .: "Foo" 
+instance FromNamedRecord Site where
+    parseNamedRecord r = Site <$> r .: "Foo" 
                               <*> r .: "Bar" 
                               <*> r .: "Woo" 
                               <*> r .: "Wibble"
@@ -34,11 +33,10 @@ main = do
     V.mapM_ print vs
 
 
-parseCsv :: String -> IO (V.Vector Test)
+parseCsv :: String -> IO (V.Vector Site)
 parseCsv fn = do
     bs <- BL.readFile fn
     case decodeByName bs of
         Left err -> undefined
-        --Right (_, v) -> V.forM v $ \ t -> return $ foo t ++ "; " ++ show (bar t) ++ "; " ++ show (woo t) ++ "; " ++ wibble t
         Right (_, v) -> return v
 
