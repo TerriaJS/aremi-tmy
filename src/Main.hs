0a3554d534a76dafe99ef70e815eba382e49ec6c
Mats Henrikson <mats@zakalwe.com>
2015-07-29 17:05:01 +1000

    Grouping records into hours.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE BangPatterns #-}
 
 -- TODO:
 -- either:
@@ -13,14 +13,17 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import Data.Csv.Streaming                   (Records(Cons, Nil))
 import Data.Either                          (partitionEithers)
+import Data.List                            (groupBy)
 import Data.Text                            (unpack)
-import Data.Time.LocalTime                  (localTimeOfDay, todMin)
+import Data.Time.LocalTime                  (LocalTime(..), TimeOfDay(..), localTimeOfDay, todMin, localDay, todHour)
 import System.Directory                     (doesFileExist)
 import System.Environment                   (getArgs)
 import System.FilePath.Find                 (find, always, (~~?), fileName)
 
 import Tmy.OneMinSolarTypes
 import Tmy.Csv
+
+import Debug.Trace
 
 
 main :: IO ()
@@ -64,12 +67,78 @@ processCsvPair fn t@(aw, sl) = do
     slRecs <- readCsv sl :: IO (Records SolarRadiationObs)
     fnExists <- doesFileExist fn
     let encOpts = defaultEncodeOptions {encIncludeHeader = not fnExists}
-        combined = combineAwSl awRecs slRecs
+        -- Records a -> [Either String a]
+        -- partitionEithers: [Either String a] -> ([String], [a])
+        (awErrs, awList) = partitionEithers (recsAsList awRecs)
+        (slErrs, slList) = partitionEithers (recsAsList slRecs)
+        -- group and compute stats for aw and sl separately
+        awGroups = groupBy (hourGrouper awLocalTime) awList
+        lwGroups = groupBy (hourGrouper slLocalTime) slList
+        !_ = traceShowId (map (map awLocalTime) (take 3 awGroups))
+        -- awFolded = map (foldl1' combine) awGroups
+        -- same for sl
+        -- combine 1-hour aw and sl records
+            -- [awFolded] -> [slFolded] -> [awSlCombined]
+        -- combined = mergeWith awLocalTime slLocalTime CombinedAwSlObs awFolded slFolded
+
+        combined = combineAwSl awRecs slRecs -- DEBUG to remove
         (lefts, rights) = partitionEithers combined
-        filtered = filter zeroMinutes rights
-    mapM_ putStrLn lefts
+        filtered = filter zeroMinutes rights -- DEBUG to remove
+    -- mapM_ putStrLn lefts
+    mapM_ putStrLn awErrs
+    mapM_ putStrLn slErrs
     BL.appendFile fn (encodeDefaultOrderedByNameWith encOpts filtered)
 
+
+hourGrouper :: (a -> LocalTime) -> a -> a -> Bool
+hourGrouper f a b = zeroedAt == zeroedBt
+    where
+        at = f a
+        bt = f b
+        atHour = (todHour . localTimeOfDay) at
+        btHour = (todHour . localTimeOfDay) bt
+        zeroedAt = LocalTime (localDay at) (TimeOfDay atHour 0 0)
+        zeroedBt = LocalTime (localDay bt) (TimeOfDay btHour 0 0)
+
+
+{-
+mergeWith :: Ord c => (a -> c)
+                   -> (b -> c)
+                   -> (Maybe a -> Maybe b -> r)
+                   -> [a] -> [b] -> [r]
+-}
+
+
+
+{-
+data Stat a = Stat {mean :: a, min :: Min a, max :: Max a, stdDev :: a, count :: Int}
+
+class Semigroup a where
+    (<>) :: a -> a -> a
+
+instance Fractional a => Semigroup (Stat a) where
+    (Stat amean amin amax adev acnt) <> (Stat bmean bmin bmax bdev bcnt) =
+        Stat ((amean * acnt' + bmean * bcnt')/(acnt'+bcnt'))
+             (amin <> bmin)
+             (amax <> bmax)
+             (joinStdDevs adev bdev acnt bcnt)
+             (acnt+bcnt)
+        where acnt' = fromIntegral acnt
+              bcnt' = fromIntegral bcnt
+
+mkStat mean min max stddev = Stat mean (Min min) (Max max) stddev 1
+
+instance Monoid (Stat a) where
+    mempty = Stat 0 mempty mempty
+    mappend
+
+    List: [] and (++)
+    Bool: True and (&&)
+    Int: 0 and (+)
+
+
+    Min a: mappend :: (Ord a, Bounded a) => Min a -> Min a -> Min a
+-}
 
 zeroMinutes :: CombinedAwSlObs -> Bool
 zeroMinutes c = (todMin . localTimeOfDay . awLocalStdTime . awRecord) c == 00
@@ -101,4 +170,3 @@ comb f (Right a) (Right b) = Right (f a b)
 comb _ (Left a) (Left b) = Left ("Two errors: '" ++ a ++ "' and '" ++ b ++ "'")
 comb _ (Left a) _ = Left ("aw side: " ++ a)
 comb _ _ (Left b) = Left ("sl side: " ++ b)
-
